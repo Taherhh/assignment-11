2d7089abcb76451da0ec20daa61a60539741d7df
Taher Shabbir <taherihh53@gmail.com>
2024-06-10 18:06:19 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//assignment no 11
+// Defines an array of names
+var names = ["Noriyah", "Baqir", "Fatima", "Hussain", "Ali"];
+//for (let i = 0; i < names.length; i++) {
+//   console.log(names[i]);
+//}
+// for each
+names.forEach(function (name) {
+    console.log(name);
+});
---
+++ main.ts
@@ -0,0 +1,12 @@
+//assignment no 11
+// Defines an array of names
+let names: string[] = ["Noriyah", "Baqir", "Fatima", "Hussain", "Ali"];
+
+//for (let i = 0; i < names.length; i++) {
+ //   console.log(names[i]);
+//}
+
+// for each
+names.forEach(name=> {
+    console.log(name);
+});
